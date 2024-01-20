ee590609c34f22895341b3dc2b47569ddac69846
miyu-di <141654479+miyu-di@users.noreply.github.com>
2024-01-20 22:50:37 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,118 @@
-console.log();
+import axios from 'axios';
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".inputsrc");
+const loader = document.querySelector(".loader");
+const gallery = document.querySelector(".gallery");
+
+const loadbtn = document.querySelector(".loadbtn");
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
+});
+
+let page = 1;
+let perPage = 40;
+let searchParams;
+
+loadbtn.addEventListener("click", loadMoreBtn());
+
+async function loadMoreBtn() {
+  await postGallery();
+}
+
+function postGallery() {
+form.addEventListener("submit", async event => {
+  event.preventDefault();
+  let userInput = input.value
+
+  searchParams = {
+    params: {
+      key: '41766309-9b727aff341ca6e5a641aa2fb',
+      q: `${userInput}`,
+      orientation: 'horizontal',
+      per_page: perPage,
+      page: page,
+      image_type: 'photo',
+      safesearch: true,
+      order: 'likes',
+    },
+  }; 
+  
+  try {
+    loader.classList.remove('hide');
+
+    const response = await axios.get(
+    'https://pixabay.com/api/',
+    searchParams);
+    
+    const images = response.data;
+    console.log(searchParams.params.page);
+
+    setTimeout(() => {
+      loader.classList.add('hide');
+      gallery.innerHTML = "";
+      if (response.data.totalHits > 0) {
+        const imglist = images.hits.reduce((html, hit) => {
+          return (
+            html +
+            `<li>
+          <a href="${hit.largeImageURL}">
+            <img src="${hit.webformatURL}" alt="${hit.tags}" class="gallery-image">
+          </a>
+          <div class="info">
+            <div class="field">
+              <span class="label">Likes</span>
+              <span class="value">${hit.likes}</span>
+            </div>
+            <div class="field">
+              <span class="label">Views</span>
+              <span class="value">${hit.views}</span>
+            </div>
+            <div class="field">
+              <span class="label">Comments</span>
+              <span class="value">${hit.comments}</span>
+            </div>
+            <div class="field">
+              <span class="label">Downloads</span>
+              <span class="value">${hit.downloads}</span>
+            </div>
+          </div>
+          </li>`
+          )
+        }, '')
+        gallery.insertAdjacentHTML('beforeend', imglist);
+        lightbox.refresh();
+        
+      } else {
+        iziToast.error({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight'
+        });
+      }
+      updateButtonVisibility();
+    }, 2000);
+  }
+    catch (error) {
+        console.log(error);
+    } finally {
+    form.reset();
+
+    }
+});
+}
+
+function updateButtonVisibility() {
+  if (gallery.innerHTML.trim() === "") {
+    loadbtn.classList.add("hide");
+  } else {
+    loadbtn.classList.remove("hide");
+  }
+}
